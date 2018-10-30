516844c03f89da02a121c99abce5955028bbce25
someshwarthuma <somesh.sandeep619@msitprogram.net>
2018-10-30 21:54:36 +0530

    "ads2_m2_a1_1: 23/24 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m2/2.1 CC_Percolation/Graph.java
+++ ADS-2-assignments/m2/2.1 CC_Percolation/Graph.java
@@ -1,73 +1,123 @@
-import java.util.Iterator;
 import java.util.NoSuchElementException;
-
 public class Graph {
     private static final String NEWLINE = System.getProperty("line.separator");
+
     private final int V;
     private int E;
-    private boolean[][] adj;
-
-    // empty Graph with V vertices
+    private Bag<Integer>[] adj;
+    
+    /**
+     * Initializes an empty graph with {@code V} vertices and 0 edges.
+     * param V the number of vertices
+     *
+     * @param  V number of vertices
+     * @throws IllegalArgumentException if {@code V < 0}
+     */
     public Graph(int V) {
-        if (V < 0) throw new IllegalArgumentException("Too few vertices");
+        if (V < 0) throw new IllegalArgumentException("Number of vertices must be nonnegative");
         this.V = V;
         this.E = 0;
-        this.adj = new boolean[V][V];
+        adj = (Bag<Integer>[]) new Bag[V];
+        for (int v = 0; v < V; v++) {
+            adj[v] = new Bag<Integer>();
+        }
+    }
+    /**
+     * Returns the number of vertices in this graph.
+     *
+     * @return the number of vertices in this graph
+     */
+    public int V() {
+        return V;
     }
 
-    // number of vertices and edges
-    public int V() { return V; }
-    public int E() { return E; }
+    /**
+     * Returns the number of edges in this graph.
+     *
+     * @return the number of edges in this graph
+     */
+    public int E() {
+        return E;
+    }
 
+    // throw an IllegalArgumentException unless {@code 0 <= v < V}
+    private void validateVertex(int v) {
+        if (v < 0 || v >= V)
+            throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V-1));
+    }
 
-    // add undirected edge v-w
+    /**
+     * Adds the undirected edge v-w to this graph.
+     *
+     * @param  v one vertex in the edge
+     * @param  w the other vertex in the edge
+     * @throws IllegalArgumentException unless both {@code 0 <= v < V} and {@code 0 <= w < V}
+     */
     public void addEdge(int v, int w) {
-        if (!adj[v][w]) E++;
-        adj[v][w] = true;
-        adj[w][v] = true;
+        validateVertex(v);
+        validateVertex(w);
+        E++;
+        adj[v].add(w);
+        adj[w].add(v);
     }
 
-    // does the Graph contain the edge v-w?
-    public boolean contains(int v, int w) {
-        return adj[v][w];
-    }
 
-    // return list of neighbors of v
+    /**
+     * Returns the vertices adjacent to vertex {@code v}.
+     *
+     * @param  v the vertex
+     * @return the vertices adjacent to vertex {@code v}, as an iterable
+     * @throws IllegalArgumentException unless {@code 0 <= v < V}
+     */
     public Iterable<Integer> adj(int v) {
-        return new AdjIterator(v);
+        validateVertex(v);
+        return adj[v];
     }
 
-    // support iteration over Graph vertices
-    private class AdjIterator implements Iterator<Integer>, Iterable<Integer> {
-        private int v;
-        private int w = 0;
-
-        AdjIterator(int v) {
-            this.v = v;
-        }
-
-        public Iterator<Integer> iterator() {
-            return this;
-        }
-
-        public boolean hasNext() {
-            while (w < V) {
-                if (adj[v][w]) return true;
-                w++;
-            }
-            return false;
-        }
-
-        public Integer next() {
-            if (!hasNext()) {
-                throw new NoSuchElementException();
-            }
-            return w++;
-        }
-
-        public void remove()  {
-            throw new UnsupportedOperationException();
-        }
+    /**
+     * Returns the degree of vertex {@code v}.
+     *
+     * @param  v the vertex
+     * @return the degree of vertex {@code v}
+     * @throws IllegalArgumentException unless {@code 0 <= v < V}
+     */
+    public int degree(int v) {
+        validateVertex(v);
+        return adj[v].size();
     }
 
+    /**
+     * Determines if it has edge.
+     *
+     * @param      v     { parameter_description }
+     * @param      w     { parameter_description }
+     *
+     * @return     True if has edge, False otherwise.
+     */
+    public boolean hasEdge(final int v, final int w) {
+        for (int i : adj[v]) {
+            if (i == w) {
+                return true;
+            }
+        }
+        return false;
+    }
+    /**
+     * Returns a string representation of this graph.
+     *
+     * @return the number of vertices <em>V</em>, followed by the number of edges <em>E</em>,
+     *         followed by the <em>V</em> adjacency lists
+     */
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        s.append(V + " vertices, " + E + " edges " + NEWLINE);
+        for (int v = 0; v < V; v++) {
+            s.append(v + ": ");
+            for (int w : adj[v]) {
+                s.append(w + " ");
+            }
+            s.append(NEWLINE);
+        }
+        return s.toString();
+    }
 }
--- ADS-2-assignments/m2/2.1 CC_Percolation/Solution.java
+++ ADS-2-assignments/m2/2.1 CC_Percolation/Solution.java
@@ -1,89 +1,174 @@
 import java.util.Scanner;
-/**.
- * Solution class
+/**
+ * Class for percolation.
  */
-class Solution {
-    /**.
-     * Constructor
+class Percolation {
+    
+    private Graph g;
+    
+    private int num;
+    /**
+     * { integer size }.
      */
-    private Solution(){
-        //solution constructor
+    private int size;
+    /**
+     * { integer begin }.
+     */
+    private int begin;
+    /**
+     * { integer end  }.
+     */
+    private int end;
+    /**
+     * { integer count }.
+     */
+    private int count;
+    /**
+     * { boolean arr  }.
+     */
+    private boolean[] arr;
+    /**
+     * Constructs the object.
+     *
+     * @param      num1     { parameter_description }.
+     */
+    Percolation(final int num1) {
+   // create n-by-n grid, with all sites blocked
+
+    this.num = num1;
+    this.size = num1 * num1;
+    this.begin = size;
+    this.count = 0;
+    this.end = size + 1;
+    arr = new boolean[size];
+    g = new Graph(size + 2);
+    for (int i = 0; i < num; i++) {
+        g.addEdge(begin, i);
+        g.addEdge(end, size - i - 1);
     }
-    /**.
-     * main method
+    }
+   /**
+    * Searches for the first match.
+    *
+    * @param      n1    The n 1
+    * @param      n2    The n 2
+    *
+    * @return     { description_of_the_return_value }.
+    */
+    private int indexOf(final int n1, final int n2) {
+        return num * (n1 - 1) + n2 - 1;
+    }
+    /**
+     * Opens sites.
+     *
+     * @param      row   The row
+     * @param      col   The col
+     */
+    private void openSites(final int row, final int col) {
+        if (arr[col] && !g.hasEdge(row, col)) {
+            g.addEdge(row, col);
+        }
+    }
+   /**
+    * { open site (row, col) if it is not open already }.
+    *
+    * @param      row   The row
+    * @param      col   The col
+    */
+    public void open(final int row, final int col) {
+        // open site (row, col) if it is not open already
+        int ind = indexOf(row, col);
+        arr[ind] = true;
+        count = count + 1;
+        int up = ind - num;
+        int down = ind + num;
+        if (num == 1) {
+            g.addEdge(begin, ind);
+            g.addEdge(end, ind);
+        }
+        if (up >= 0) {
+            openSites(ind, up);
+        }
+        if (down < size) {
+            openSites(ind, down);
+        }
+
+        if (col == 1) {
+            if (col != num) {
+                openSites(ind, ind + 1);
+            }
+            return;
+        }
+        if (col == num) {
+            openSites(ind, ind - 1);
+            return;
+        }
+        // if (num == 2) {
+        //     weigh.union(ind, ind + 1);
+        //     weigh.union(ind, );
+        //     // return;
+        // }
+        openSites(ind, ind + 1);
+        openSites(ind, ind - 1);
+    }
+   /**
+    * Determines if open.
+    *
+    * @param      row   The row
+    * @param      col   The col
+    *
+    * @return     True if open, False otherwise.
+    */
+    public boolean isOpen(final int row, final int col) {
+        // is site (row, col) open?
+        return arr[indexOf(row, col)];
+    }
+    /**
+    * { function_description }.
+    *
+    * @return     { description_of_the_return_value }
+    */
+    public int numberOfOpenSites() {
+        // number of open sites
+
+        return count;
+    }
+   /**
+    * { function_description }.
+    *
+    * @return     { description_of_the_return_value }
+    */
+    public boolean percolates() {
+        CC conn = new CC(g);
+        return conn.connected(begin, end);
+    }
+}
+
+/**
+ * Client program.
+ */
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        // constructor not used.
+    }
+    /**
+     * Main class.
      *
      * @param      args  The arguments
      */
-    public static void main(String[] args) {
-        /**
-         * vertex variable
-         */
-        //private int vertex;
-        Scanner sc = new Scanner(System.in);
-        int vertex = sc.nextInt();
-        Percolation p = new Percolation(vertex);
-        boolean[][] grid = new boolean[vertex][vertex];
-        Graph gr = new Graph(vertex * vertex + 2);
-        try {
-            while (true) {
-                int row = sc.nextInt(), col = sc.nextInt();
-                grid[row - 1][col - 1] = true;
-            }
-        } catch (Exception e) {
-        } finally {
-            System.out.println(p.checkPerc(grid, gr));
+    public static void main(final String[] args) {
+        Scanner s = new Scanner(System.in);
+        int num = Integer.parseInt(s.nextLine());
+        Percolation pobj = new Percolation(num);
+        while (s.hasNext()) {
+            String[] tokens = s.nextLine().split(" ");
+            pobj.open(Integer.parseInt(tokens[0]),
+                Integer.parseInt(tokens[1]));
         }
+        System.out.println(pobj.percolates()
+            && pobj.numberOfOpenSites() != 0);
     }
 }
-/**.
- * percolation class
- */
-class Percolation {
-    private int vertex;
-    Percolation(int ertex) {
-        this.vertex = ertex;
-    }
-    public boolean checkPerc(boolean[][] grid , Graph gr) {
-        for (int i = 0 ; i < vertex ; i++) {
-            for (int j = 0 ; j < vertex; j++) {
-                if (grid[i][j]) {
-
-                    int tmp = trans(i, j);
-                    if (i == 0) {
-                        gr.addEdge(tmp , vertex * vertex);
-                    }
-                    if (i == vertex - 1) {
-                        gr.addEdge(tmp , vertex * vertex + 1);
-                    }
-                    if (i - 1 >= 0 && grid[i - 1][j]) {
-                        gr.addEdge(tmp , trans(i - 1, j));
-                    }
-                    if (i + 1 < vertex  && grid[i + 1][j]) {
-                        gr.addEdge(tmp , trans(i + 1, j));
-                    }
-                    if (j - 1 >= 0 && grid[i][j - 1]) {
-                        gr.addEdge(tmp , trans(i, j - 1));
-                    }
-                    if (j + 1 < vertex && grid[i][j + 1]) {
-                        gr.addEdge(tmp , trans(i, j + 1));
-                    }
-                }
-            }
-        }
-        DepthFirstPaths d = new DepthFirstPaths(gr, vertex * vertex);
-        return d.hasPathTo(vertex * vertex + 1);
-    }
-    /**.
-     * methdo to translate the 2-d array to one d array
-     *
-     * @param      i     { index }
-     * @param      j     { index }
-     *
-     * @return     { int }
-     */
-    private int trans(int i , int j) {
-        return i * vertex + j;
-    }
-}
-
-
