99f6f82e301af46e6605b1a083b72d3cb4769556
someshwarthuma <somesh.sandeep619@msitprogram.net>
2018-10-30 20:35:05 +0530

    "ads2_m2_a1_1: 24/24 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m2/2.1 CC_Percolation/Solution.java
+++ ADS-2-assignments/m2/2.1 CC_Percolation/Solution.java
@@ -1,9 +1,26 @@
 import java.util.Scanner;
+/**.
+ * Solution class
+ */
 class Solution {
+    /**.
+     * Constructor
+     */
+    private Solution(){
+        //solution constructor
+    }
+    /**.
+     * main method
+     *
+     * @param      args  The arguments
+     */
     public static void main(String[] args) {
-        int vertex;
+        /**
+         * vertex variable
+         */
+        //private int vertex;
         Scanner sc = new Scanner(System.in);
-        vertex = sc.nextInt();
+        int vertex = sc.nextInt();
         Percolation p = new Percolation(vertex);
         boolean[][] grid = new boolean[vertex][vertex];
         Graph gr = new Graph(vertex * vertex + 2);
@@ -18,8 +35,11 @@ class Solution {
         }
     }
 }
+/**.
+ * percolation class
+ */
 class Percolation {
-    int vertex;
+    private int vertex;
     Percolation(int ertex) {
         this.vertex = ertex;
     }
@@ -29,18 +49,38 @@ class Percolation {
                 if (grid[i][j]) {
 
                     int tmp = trans(i, j);
-                    if (i == 0) gr.addEdge(tmp , vertex * vertex);
-                    if (i == vertex - 1)gr.addEdge(tmp , vertex * vertex + 1);
-                    if (i - 1 >= 0 && grid[i - 1][j]) gr.addEdge(tmp , trans(i - 1, j));
-                    if (i + 1 < vertex  && grid[i + 1][j]) gr.addEdge(tmp , trans(i + 1, j));
-                    if (j - 1 >= 0 && grid[i][j - 1]) gr.addEdge(tmp , trans(i, j - 1));
-                    if (j + 1 < vertex && grid[i][j + 1]) gr.addEdge(tmp , trans(i, j + 1));
+                    if (i == 0) {
+                        gr.addEdge(tmp , vertex * vertex);
+                    }
+                    if (i == vertex - 1) {
+                        gr.addEdge(tmp , vertex * vertex + 1);
+                    }
+                    if (i - 1 >= 0 && grid[i - 1][j]) {
+                        gr.addEdge(tmp , trans(i - 1, j));
+                    }
+                    if (i + 1 < vertex  && grid[i + 1][j]) {
+                        gr.addEdge(tmp , trans(i + 1, j));
+                    }
+                    if (j - 1 >= 0 && grid[i][j - 1]) {
+                        gr.addEdge(tmp , trans(i, j - 1));
+                    }
+                    if (j + 1 < vertex && grid[i][j + 1]) {
+                        gr.addEdge(tmp , trans(i, j + 1));
+                    }
                 }
             }
         }
         DepthFirstPaths d = new DepthFirstPaths(gr, vertex * vertex);
         return d.hasPathTo(vertex * vertex + 1);
     }
+    /**.
+     * methdo to translate the 2-d array to one d array
+     *
+     * @param      i     { index }
+     * @param      j     { index }
+     *
+     * @return     { int }
+     */
     private int trans(int i , int j) {
         return i * vertex + j;
     }
