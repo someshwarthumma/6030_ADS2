21c16150aedda53600427a5168a26095502ae620
someshwarthuma <somesh.sandeep619@msitprogram.net>
2018-10-31 22:51:48 +0530

    "ads2_m3_a2_1: 3/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

---
+++ ADS-2-assignments/m3/3.2 Bipartite/Solution.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+class Solution{
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int vertices = Integer.parseInt(s.nextLine());
+		int edges = Integer.parseInt(s.nextLine());
+		Graph g = new Graph(vertices);
+		Bipartite b = new Bipartite(g);
+		if (b.isBipartitie()) {
+			System.out.println("Graph is bipartite");
+		} else {
+			System.out.println("Graph is not a bipartite");
+		}
+	}
+}
+
+class Bipartite {
+	boolean[] marked;
+	boolean[] color;
+	int[] edgeTo;
+	Stack<Integer> stack;
+	boolean flag;
+	Bipartite(Graph g) {
+		marked = new boolean[g.vertices()];
+		color = new boolean[g.vertices()];
+		edgeTo = new int[g.vertices()];
+		flag = true;
+		for (int i = 0 ; i < g.vertices(); i++) {
+			if (!marked[i]) {
+				dfs(g, i);
+			}
+		}
+	}
+
+	public void dfs(Graph g, int vertex) {
+		marked[vertex] = true;
+		/*if(flag==false){
+			return;
+		}*/
+		for (int each : g.adj(vertex)) {
+			if (!marked[each]) {
+				color[each] = !color[vertex];
+				edgeTo[each] = vertex;
+				dfs(g, each);
+			} else if (color[each]==color[vertex]){
+				flag = false;
+			}
+		}
+	}
+
+	public boolean isBipartitie(){
+		return flag;
+	}
+
+
+}
