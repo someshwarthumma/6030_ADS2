f76a2a8a13addff5730eb3c2f773f7f5e58c25c7
someshwarthuma <somesh.sandeep619@msitprogram.net>
2018-11-04 20:08:23 +0530

    "ads2_m6_a1_1: 5/12 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m6/ADS-2Week-1/Solution.java
+++ ADS-2-assignments/m6/ADS-2Week-1/Solution.java
@@ -5,108 +5,108 @@ import java.util.Scanner;
  * class to find the pagerank
  */
 class PageRank {
-	/**.
-	 * graph as g
-	 */
-	Digraph g;
-	/**.
-	 * reverse of the given graph as revG
-	 */
-	Digraph revG;
-	/**.
-	 * variable for vertices
-	 */
-	int vertices;
-	/**.
-	 * array to store the pageRanks
-	 */
-	Double[] pgRank;
-	//HashMap <Integer, ArrayList<Integer>> revMap;
+    /**.
+     * graph as g
+     */
+    private Digraph g;
+    /**.
+     * reverse of the given graph as revG
+     */
+    private Digraph revG;
+    /**.
+     * variable for vertices
+     */
+    private int vertices;
+    /**.
+     * array to store the pageRanks
+     */
+    private Double[] pgRank;
+    //HashMap <Integer, ArrayList<Integer>> revMap;
 
-	/**.
-	 * constructor
-	 *
-	 * @param      gr    The graphics
-	 */
-	PageRank(Digraph gr) {
-		//this.revMap = revMap;
-		this.g = gr;
-		this.revG = g.reverse();
-		this.vertices = g.V();
-		pgRank = new Double[vertices];
-		int ver = g.V();
-		for (int i = 0; i < vertices; i++) {
-			//System.out.println("Vertex: "+ i);
-			pgRank[i] = 1.0 / ver;
-		}
-		calcPageRank();
-	}
-	/**.
-	 * method to calculate the page Rank
-	 */
-	public void calcPageRank() {
-		for (int i = 0; i < vertices; i++) {
-			if (g.outdegree(i) == 0) {
-				for (int j = 0; j < vertices ; j++) {
-					if (i != j) {
-						g.addEdge(i, j);
-					}
-				}
-			}
-		}
-		//int count = 0;
+    /**.
+     * constructor
+     *
+     * @param      gr    The graphics
+     */
+    PageRank(final Digraph gr) {
+        //this.revMap = revMap;
+        this.g = gr;
+        this.revG = g.reverse();
+        this.vertices = g.V();
+        pgRank = new Double[vertices];
+        int ver = g.V();
+        for (int i = 0; i < vertices; i++) {
+            //System.out.println("Vertex: "+ i);
+            pgRank[i] = 1.0 / ver;
+        }
+        calcPageRank();
+    }
+    /**.
+     * method to calculate the page Rank
+     */
+    public void calcPageRank() {
+        for (int i = 0; i < vertices; i++) {
+            if (g.outdegree(i) == 0) {
+                for (int j = 0; j < vertices; j++) {
+                    if (i != j) {
+                        g.addEdge(i, j);
+                    }
+                }
+            }
+        }
+        //int count = 0;
+        final int thou = 1000;
+        for (int k = 1; k < thou; k++) {
+            /*if (count == 5) {
+                break;
+            }*/
+            Double[] tempPR = new Double[vertices];
+            for (int i = 0; i < vertices; i++) {
+                //ArrayList<Integer> inList = revMap.get(i);
+                Double sum = 0.0;
 
-		for (int k = 1; k < 1000; k++) {
-			/*if (count == 5) {
-				break;
-			}*/
-			Double[] tempPR = new Double[vertices];
-			for (int i = 0; i < vertices; i++) {
-				//ArrayList<Integer> inList = revMap.get(i);
-				Double sum = 0.0;
-
-				//int listSize = inList.size();
-				for (int each : g.reverse().adj(i)) {
-					sum = sum +
-					pgRank[each] / g.outdegree(each);
-				}
-				tempPR[i] = sum;
-				//System.out.println("for i: "
-				//+i+"-- sum: "+sum);
-				//pgRank[i] = sum;
-			}
-			//System.out.println("PGRANk: "
-			//+Arrays.toString(pgRank));
-			//System.out.println("tempPR: "
-			//+Arrays.toString(tempPR));
-			/*if (pgRank.equals(tempPR)) {
-				System.out.println("equals");
-				count++;
-			}*/
-			pgRank = tempPR;
-			// System.out.println("for k: "+k+" -- Array
-			//: "+Arrays.toString(pgRank));
-		}
-	}
-	/**.
-	 * method to get the page rank for the given page rank
-	 *
-	 * @param      v     { vertices of type int }
-	 *
-	 * @return     The page rank.
-	 */
-	public Double getPageRank(final int v) {
-		//calcPageRank();
-		return pgRank[v];
-	}
-	/**.
-	 * method to printer
-	 */
-	public void printer() {
-		for (int i = 0; i < vertices; i++) {
-			System.out.println(i + " - " + pgRank[i]);
-		}
-	}
+                //int listSize = inList.size();
+                for (int each : g.reverse().adj(i)) {
+                    sum = sum
+                    +pgRank[each] / g.outdegree(each);
+                }
+                tempPR[i] = sum;
+                //System.out.println("for i: "
+                //+i+"-- sum: "+sum);
+                //pgRank[i] = sum;
+            }
+            //System.out.println("PGRANk: "
+            //+Arrays.toString(pgRank));
+            //System.out.println("tempPR: "
+            //+Arrays.toString(tempPR));
+            /*if (pgRank.equals(tempPR)) {
+                System.out.println("equals");
+                count++;
+            }*/
+            pgRank = tempPR;
+            // System.out.println("for k: "+k+" -- Array
+            //: "+Arrays.toString(pgRank));
+        }
+    }
+    /**.
+     * method to get the page rank for the given page rank
+     *
+     * @param      v     { vertices of type int }
+     *
+     * @return     The page rank.
+     */
+    public Double getPageRank(final int v) {
+        //calcPageRank();
+        return pgRank[v];
+    }
+    /**.
+     * method to printer
+     */
+    public void printer() {
+        for (int i = 0; i < vertices; i++) {
+            System.out.println(i + " - " + pgRank[i]);
+        }
+    }
 }
 /**.
  * class to for web search
@@ -119,88 +119,88 @@ class WebSearch {
  * solution class
  */
 final class Solution {
-	/**.
-	 * constructor
-	 */
-	private Solution() {
-		//Constructor
-	}
-	/**.
-	 * main method to handle the input testcases
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		/**.
-		 * Scanner object
-		 */
-		Scanner s = new Scanner(System.in);
-		// read the first line of the 
-		// input to get the number of vertices
-		//ArrayList<Bag<Integer>> list =
-		//new ArrayList<Bag<Integer>>();
+    /**.
+     * constructor
+     */
+    private Solution() {
+        //Constructor
+    }
+    /**.
+     * main method to handle the input testcases
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        /**.
+         * Scanner object
+         */
+        Scanner s = new Scanner(System.in);
+        // read the first line of the
+        // input to get the number of vertices
+        //ArrayList<Bag<Integer>> list =
+        //new ArrayList<Bag<Integer>>();
 
-		int noOfVertices = Integer.parseInt(s.nextLine());
-		Digraph g = new Digraph(noOfVertices);
-		//int count=0;
-		for (int i = 0; i < noOfVertices; i++) {
-			String[] tokens = s.nextLine().split(" ");
-			//Bag<Integer> bag = new Bag<Integer>();
-			for (int j = 1; j < tokens.length; j++) {
-				int id = Integer.parseInt(tokens[0]);
-				int child = Integer.parseInt(tokens[j]);
-				/*if (revMap.containsKey(child)) {
-					ArrayList<Integer> arr = 
-					revMap.get(child);
-					if (!arr.contains(id)) {
-						arr.add(id);
-						revMap.put(child, arr);
-					}
-				} else {
-					ArrayList<Integer> n = 
-					new ArrayList<Integer>();
-					n.add(id);
-					revMap.put(child, n);
-				}*/
-				g.addEdge(id, child);
-				//count++;
-				//bag.add(j);
-			}
-			//list.add(bag);
+        int noOfVertices = Integer.parseInt(s.nextLine());
+        Digraph g = new Digraph(noOfVertices);
+        //int count=0;
+        for (int i = 0; i < noOfVertices; i++) {
+            String[] tokens = s.nextLine().split(" ");
+            //Bag<Integer> bag = new Bag<Integer>();
+            for (int j = 1; j < tokens.length; j++) {
+                int id = Integer.parseInt(tokens[0]);
+                int child = Integer.parseInt(tokens[j]);
+                /*if (revMap.containsKey(child)) {
+                    ArrayList<Integer> arr =
+                    revMap.get(child);
+                    if (!arr.contains(id)) {
+                        arr.add(id);
+                        revMap.put(child, arr);
+                    }
+                } else {
+                    ArrayList<Integer> n =
+                    new ArrayList<Integer>();
+                    n.add(id);
+                    revMap.put(child, n);
+                }*/
+                g.addEdge(id, child);
+                //count++;
+                //bag.add(j);
+            }
+            //list.add(bag);
 
-		}
-		// iterate count of vertices times
-		// to read the adjacency list from std input
-		// and build the graph
+        }
+        // iterate count of vertices times
+        // to read the adjacency list from std input
+        // and build the graph
 
-		//System.out.println(
-		//noOfVertices+" vertices, "+count+" edges");
-		//System.out.println("ITs done");
-		System.out.println(g.toString());
-		//System.out.println("ITs done");
+        //System.out.println(
+        //noOfVertices+" vertices, "+count+" edges");
+        //System.out.println("ITs done");
+        System.out.println(g.toString());
+        //System.out.println("ITs done");
 
-		PageRank pageRankObj = new PageRank(g);
+        PageRank pageRankObj = new PageRank(g);
 
 
-		// Create page rank object to
-		//pass the graph object to the constructor
-		pageRankObj.printer();
+        // Create page rank object to
+        //pass the graph object to the constructor
+        pageRankObj.printer();
 
-		// print the page rank object
+        // print the page rank object
 
-		// This part is only for the final test case
+        // This part is only for the final test case
 
-		// File path to the web content
-		String file = "WebContent.txt";
+        // File path to the web content
+        String file = "WebContent.txt";
 
-		// instantiate web search object
-		// and pass the page rank object
-		// and the file path to the constructor
+        // instantiate web search object
+        // and pass the page rank object
+        // and the file path to the constructor
 
-		// read the search queries from std in
-		// remove the q= prefix and extract the search word
-		// pass the word to iAmFeelingLucky method of web search
-		// print the return value of iAmFeelingLucky
+        // read the search queries from std in
+        // remove the q= prefix and extract the search word
+        // pass the word to iAmFeelingLucky method of web search
+        // print the return value of iAmFeelingLucky
 
-	}
+    }
 }
