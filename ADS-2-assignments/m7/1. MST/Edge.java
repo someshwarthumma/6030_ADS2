a817e4396fad01b93e7247f4df303577fcbdbc42
someshwarthuma <somesh.sandeep619@msitprogram.net>
2018-11-05 21:58:43 +0530

    "ads2_m7_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m7/1. MST/Edge.java
+++ ADS-2-assignments/m7/1. MST/Edge.java
@@ -1,26 +1,68 @@
-class Edge implements Comparable<Edge>{
-	private int firstVertex;
-	private int secondVertex;
-	private Double weight;
-	Edge(final int v, final int w, final Double wei){
-		this.firstVertex = v;
-		this.secondVertex = w;
-		this.weight = wei;
-	}
-
-	public Double weight(){
-		return this.weight;
-	}
-	public int either(){
-		return firstVertex;
-	}
-	public int other(int v){
-		if(firstVertex == v){
-			return secondVertex;
-		}
-		return firstVertex;
-	}
-	public int compareTo(Edge that){
-		return this.weight().compareTo(that.weight());
-	}
+/**.
+ * Edge class
+ */
+class Edge implements Comparable<Edge> {
+    /**.
+     * variable for first vertex
+     */
+    private int firstVertex;
+    /**.
+     * variable for sedcond vertex
+     */
+    private int secondVertex;
+    /**.
+     * variable for weight
+     */
+    private Double weight;
+    /**.
+     * constructor
+     *
+     * @param      v     { of type int }
+     * @param      w     { of type int }
+     * @param      wei   The wei of type dounle
+     */
+    Edge(final int v, final int w, final Double wei) {
+        this.firstVertex = v;
+        this.secondVertex = w;
+        this.weight = wei;
+    }
+    /**.
+     * method to return the weight
+     *
+     * @return     { Double }
+     */
+    public Double weight() {
+        return this.weight;
+    }
+    /**.
+     * either method to return one vertex
+     *
+     * @return     { int }
+     */
+    public int either() {
+        return firstVertex;
+    }
+    /**.
+     * other method to find the other vertex
+     *
+     * @param      v     { of type int }
+     *
+     * @return     { int }
+     */
+    public int other(final int v) {
+        if (firstVertex == v) {
+            return secondVertex;
+        }
+        return firstVertex;
+    }
+    /**.
+     * method to compare the two Edge objects
+     *
+     * @param      that  The that
+     *
+     * @return     { int }
+     */
+    public int compareTo(Edge that) {
+        return this.weight().compareTo(that.weight());
+    }
 }
