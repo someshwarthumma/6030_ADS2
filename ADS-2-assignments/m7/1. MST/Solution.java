9a485a93f5dc9537209d0adb79828b41ed41f08b
someshwarthuma <somesh.sandeep619@msitprogram.net>
2018-11-05 19:02:19 +0530

    "ads2_m7_a1_1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m7/1. MST/Solution.java
+++ ADS-2-assignments/m7/1. MST/Solution.java
@@ -10,7 +10,7 @@ class Solution{
 			g.addEdge(new Edge(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[1]), Double.parseDouble(tokens[2])));
 		}
 		KrushkalMST krushkal = new KrushkalMST(g);
-		System.out.println(krushkal.totalWeight());
+		System.out.format("%.5f", krushkal.totalWeight());
 
 	}
 }
