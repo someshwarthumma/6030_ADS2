24417ae45937fc662eb7ef5c58fa4d2ddb587792
someshwarthuma <somesh.sandeep619@msitprogram.net>
2018-11-05 22:08:05 +0530

    "ads2_m7_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2-assignments/m7/1. MST/UF.java
+++ ADS-2-assignments/m7/1. MST/UF.java
@@ -1,11 +1,11 @@
-﻿/**
+/**
  * Class for uf.
  */
 public class UF {
     /**
      * parent array.
      */
-    private int[] parent;  // parent[i] = parent of i
+    private int[] parent;
     /**
      * rank array.
      */
@@ -14,15 +14,18 @@ public class UF {
      * count.
      */
     private int count;     // number of components
-
     /**
      * Initializes an empty union–find data structure with {@code n} sites
      * {@code 0} through {@code n-1}. Each site is initially in its own
      * component.
      *
      * @param  n the number of sites
+     * @throws IllegalArgumentException if {@code n < 0}
      */
     public UF(final int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException();
+        }
         count = n;
         parent = new int[n];
         rank = new byte[n];
@@ -31,26 +34,31 @@ public class UF {
             rank[i] = 0;
         }
     }
+
     /**
-     * Returns the component identifier for the
-     * component containing site {@code p}.
-     * @param  par the integer representing one site
-     * @return the component identifier for the
-     * component containing site {@code p}
+     * Returns the component identifier for the component
+     * containing site {@code p}.
+     * @param  p the integer representing one site
+     * @return the component identifier for the component
+     * containing site {@code p}
+     * @throws IllegalArgumentException unless {@code 0 <= p < n}
+     * Time complexity for this method is O(p).
      */
-    public int find(final int par) {
-        int p = par;
-        while (p != parent[p]) {
-            parent[p] = parent[parent[p]];
-            p = parent[p];
+    public int find(final int p) {
+        int p1 = p;
+        validate(p1);
+        while (p1 != parent[p1]) {
+            parent[p1] = parent[parent[p1]];    // path compression by halving
+            p1 = parent[p1];
         }
-        return p;
+        return p1;
     }
 
     /**
      * Returns the number of components.
      *
      * @return the number of components (between {@code 1} and {@code n})
+     * Time complexity for this method is O(1).
      */
     public int count() {
         return count;
@@ -61,9 +69,12 @@ public class UF {
      *
      * @param  p the integer representing one site
      * @param  q the integer representing the other site
-     * @return {@code true} if the two sites {@code p} and {@code q}
-     * are in the same component;
+     * @return {@code true} if the two sites {@code p}
+     * and {@code q} are in the same component;
      *         {@code false} otherwise
+     * @throws IllegalArgumentException unless
+     *         both {@code 0 <= p < n} and {@code 0 <= q < n}
+     * Time complexity for this method is O(1).
      */
     public boolean connected(final int p, final int q) {
         return find(p) == find(q);
@@ -75,6 +86,9 @@ public class UF {
      *
      * @param  p the integer representing one site
      * @param  q the integer representing the other site
+     * @throws IllegalArgumentException unless
+     *         both {@code 0 <= p < n} and {@code 0 <= q < n}
+     * Time complexity for this method is O(log E).
      */
     public void union(final int p, final int q) {
         int rootP = find(p);
@@ -93,5 +107,16 @@ public class UF {
         }
         count--;
     }
-}
 
+    /**
+     * validate method.
+     * @param      p     one site.
+     */
+    private void validate(final int p) {
+        int n = parent.length;
+        if (p < 0 || p >= n) {
+            throw new IllegalArgumentException("index " + p
+             + " is not between 0 and " + (n - 1));
+        }
+    }
+}
