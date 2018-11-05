981abee6fbb6abc8a37188e2ffe93044f4ae0924
someshwarthuma <somesh.sandeep619@msitprogram.net>
2018-11-05 22:16:08 +0530

    "ads2_m7_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2-assignments/m7/1. MST/KruskalMST.java
+++ ADS-2-assignments/m7/1. MST/KruskalMST.java
@@ -2,52 +2,52 @@
  * krushkal class for mst implimentation
  */
 class KrushkalMST {
-	/**.
-	 * weight variable
-	 */
-	Double weight = 0.0;
-	/**.
-	 * Queue for implementing mst
-	 */
-	private Queue<Edge> mst = new Queue<Edge>();
-	/**.
-	 * Krushkal constructor
-	 *
-	 * @param      g     { of type edge eighted graph }
-	 */
-	KrushkalMST(final EdgeWeightedGraph g) {
-		//weight = 0.0;
-		MinPQ<Edge> pq = new MinPQ<Edge>();
-		for (Edge e : g.edges()) {
-			pq.insert(e);
-		}
-		UF uf = new UF(g.vertices());
-		while (!pq.isEmpty() && mst.size() < g.vertices() - 1) {
-			Edge e = pq.delMin();
-			int v = e.either();
-			int w = e.other(v);
-			if (!uf.connected(v, w)) {
-				uf.union(v, w);
-				//this is mst
-				mst.enqueue(e);
-				weight += e.weight();
-			}
-		}
-	}
-	/**.
-	 * method to get the total weight
-	 *
-	 * @return     { Double }
-	 */
-	public Double totalWeight() {
-		return weight;
-	}
-	/**.
-	 * method to return the edges
-	 *
-	 * @return     { Iterator }
-	 */
-	public Iterable<Edge> edges() {
-		return mst;
-	}
+    /**.
+     * weight variable
+     */
+    private Double weight = 0.0;
+    /**.
+     * Queue for implementing mst
+     */
+    private Queue<Edge> mst = new Queue<Edge>();
+    /**.
+     * Krushkal constructor
+     *
+     * @param      g     { of type edge eighted graph }
+     */
+    KrushkalMST(final EdgeWeightedGraph g) {
+        //weight = 0.0;
+        MinPQ<Edge> pq = new MinPQ<Edge>();
+        for (Edge e : g.edges()) {
+            pq.insert(e);
+        }
+        UF uf = new UF(g.vertices());
+        while (!pq.isEmpty() && mst.size() < g.vertices() - 1) {
+            Edge e = pq.delMin();
+            int v = e.either();
+            int w = e.other(v);
+            if (!uf.connected(v, w)) {
+                uf.union(v, w);
+                //this is mst
+                mst.enqueue(e);
+                weight += e.weight();
+            }
+        }
+    }
+    /**.
+     * method to get the total weight
+     *
+     * @return     { Double }
+     */
+    public Double totalWeight() {
+        return weight;
+    }
+    /**.
+     * method to return the edges
+     *
+     * @return     { Iterator }
+     */
+    public Iterable<Edge> edges() {
+        return mst;
+    }
 }
