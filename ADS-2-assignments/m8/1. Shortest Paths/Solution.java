60015abe3e3167203db323a65904b6e7344727e2
someshwarthuma <somesh.sandeep619@msitprogram.net>
2018-11-06 18:56:52 +0530

    "ads2_m8_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2-assignments/m8/1. Shortest Paths/Solution.java
+++ ADS-2-assignments/m8/1. Shortest Paths/Solution.java
@@ -1,6 +1,14 @@
 import java.util.Scanner;
 import java.util.HashMap;
-class Solution{
+/**
+ * solution class
+ */
+class Solution {
+	/**.
+	 * main method
+	 *
+	 * @param      args  The arguments
+	 */
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		String[] details = s.nextLine().split(" ");
@@ -8,20 +16,26 @@ class Solution{
 		int edges = Integer.parseInt(details[1]);
 		HashMap<String, Integer> map = new HashMap<String, Integer>(vertices);
 		String[] tokens = s.nextLine().split(" ");
-		for(int i = 0; i< vertices; i++){
+		for (int i = 0; i < vertices; i++) {
 			map.put(tokens[i], i);
 		}
 		EdgeWeightedGraph graph = new EdgeWeightedGraph(vertices);
-		for(int i =0; i< edges; i++){
+		for (int i = 0; i < edges; i++) {
 			String[] tok = s.nextLine().split(" ");
 			graph.addEdge(new Edge(map.get(tok[0]), map.get(tok[1]), Double.parseDouble(tok[2])));
 		}
-		operations(graph , s, map);
+		shortestpath(graph , s, map);
 	}
-
-	public static void operations(EdgeWeightedGraph g, Scanner s, HashMap<String, Integer> map){
+	/**.
+	 * method to read the query and peform the operation
+	 *
+	 * @param      g     { EdgeWeightedGraph }
+	 * @param      s     { scanner object }
+	 * @param      map   Hash map
+	 */
+	public static void shortestpath(EdgeWeightedGraph g, Scanner s, HashMap<String, Integer> map) {
 		int noOfQueries = Integer.parseInt(s.nextLine());
-		for(int i = 0; i< noOfQueries; i++){
+		for (int i = 0; i < noOfQueries; i++) {
 			String[] tok = s.nextLine().split(" ");
 			DijkstraUndirectedSP sp = new DijkstraUndirectedSP(g, map.get(tok[0]));
 			System.out.println((int)sp.distTo(map.get(tok[1])));
