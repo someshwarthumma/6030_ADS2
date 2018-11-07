9ab6469dfb53098979a69d03e8ad0f34a39ed320
someshwarthuma <somesh.sandeep619@msitprogram.net>
2018-11-07 17:59:49 +0530

    "ads2_m4_a1_1: 5/15 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m4/Code Camp - WordNet/WordNet/Solution.java
+++ ADS-2-assignments/m4/Code Camp - WordNet/WordNet/Solution.java
@@ -26,6 +26,27 @@ public final class Solution {
                 System.out.println(e.getMessage());
             }
             break;
+        case "Queres":
+            try {
+                WordNet wordnet1 = new WordNet(synsets,
+                                          hypernyms);
+                while (StdIn.hasNextLine()) {
+                    String queryNoun = StdIn.readLine();
+                    String[] queryArray = queryNoun.split(" ");
+                    if (queryArray[0].equals("null")) {
+                        throw new IllegalArgumentException(
+                            "IllegalArgumentException");
+                    }
+                    System.out.println("distance = "
+                        + wordnet1.distance(queryArray[0],
+                        queryArray[1]) + ", ancestor = "
+                        + wordnet1.sap(queryArray[0], queryArray[1]));
+                }
+            } catch (Exception e) {
+                //e.printStackTrace();
+                System.out.println(e.getMessage());
+            }
+            break;
         default:
             break;
         }
