e97b46674e484ddab0604ef859436aac77debdc4
someshwarthuma <somesh.sandeep619@msitprogram.net>
2018-11-10 11:10:53 +0530

    "ads2_m12_a1_1: 4/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

---
+++ ADS-2-assignments/m12/ADS-2_Exam-2/Solution.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+public class Solution {
+
+	public static void main(String[] args) {
+		// Self loops are not allowed...
+		// Parallel Edges are allowed...
+		// Take the Graph input here...
+		Scanner s = new Scanner(System.in);
+		int noOfCities = Integer.parseInt(s.nextLine());
+		int noOfRoads = Integer.parseInt(s.nextLine());
+		//System.out.println("Cities: "+noOfCities+" Roads: "+noOfRoads);
+		EdgeWeightedGraph graph = new EdgeWeightedGraph(noOfCities);
+		for(int i =0; i< noOfRoads;i++){
+			String[] tok = s.nextLine().split(" ");
+			graph.addEdge(new Edge(Integer.parseInt(tok[0]),
+				Integer.parseInt(tok[1]),
+				Double.parseDouble(tok[2])));
+		}
+
+		String caseToGo = s.nextLine();
+		switch (caseToGo) {
+		case "Graph":
+			//Print the Graph Object.
+			System.out.println(graph.toString());
+			break;
+
+		case "DirectedPaths":
+			// Handle the case of DirectedPaths, where two integers are given.
+			// First is the source and second is the destination.
+			// If the path exists print the distance between them.
+			// Other wise print "No Path Found."
+			String[] data = s.nextLine().split(" ");
+			int from = Integer.parseInt(data[0]);
+			int to = Integer.parseInt(data[1]);
+			DijkstraUndirectedSP dij = new DijkstraUndirectedSP(graph , from);
+			if(dij.hasPathTo(to)){
+				Double sum = 0.0;
+				for(Edge e: dij.pathTo(to)){
+					sum += e.weight();
+				}
+				System.out.println(sum);
+			} else {
+				System.out.println("No Path Found.");
+			}
+			break;
+
+		case "ViaPaths":
+			// Handle the case of ViaPaths, where three integers are given.
+			// First is the source and second is the via is the one where path should pass throuh.
+			// third is the destination.
+			// If the path exists print the distance between them.
+			// Other wise print "No Path Found."
+			break;
+
+		default:
+			break;
+		}
+
+	}
+}
