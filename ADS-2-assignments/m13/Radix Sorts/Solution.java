f822310ff9041d9b3239bb8319dc25beaf571954
someshwarthuma <somesh.sandeep619@msitprogram.net>
2018-11-12 21:58:34 +0530

    "ads2_m13_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m13/Radix Sorts/Solution.java
+++ ADS-2-assignments/m13/Radix Sorts/Solution.java
@@ -1,47 +1,76 @@
 import java.util.Scanner;
 import java.util.Arrays;
-class Solution {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int num = Integer.parseInt(scan.nextLine());
-		String[] arr = new String[num];
-		for(int i =0; i< num; i++){
-			arr[i] = scan.nextLine();
-		}
-		int length = arr[0].length();
-		LSD lsd = new LSD();
-		lsd.sort(arr, length);
-		System.out.println(Arrays.toString(arr));
+/**.
+ * solution class
+ */
+final class Solution {
+    /**.
+     * Solution constructor
+     */
+    private Solution() {
+        //Constructor
+    }
+    /**.
+     * main methdo to handle the input testcases
+     * time complexity is o(N) + O(W + N)
+     * as it reads the inputs and sort
+     *
+     * @param      args  The arguments
+     */
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num = Integer.parseInt(scan.nextLine());
+        String[] arr = new String[num];
+        for (int i = 0; i < num; i++) {
+            arr[i] = scan.nextLine();
+        }
+        int length = arr[0].length();
+        LSD lsd = new LSD();
+        lsd.sort(arr, length);
+        System.out.println(Arrays.toString(arr));
 
-	}
+    }
 
 }
-
+/**.
+ * class to implement the lsd sort
+ * time complexity is O(W *N)
+ * W is the fixed length
+ * N is the array length
+ */
 class LSD {
-	public static void sort(String[] a, int w) {
-		int n = a.length;
-		int R = 256;   // extend ASCII alphabet size
-		String[] aux = new String[n];
-
-		for (int d = w - 1; d >= 0; d--) {
-			// sort by key-indexed counting on dth character
-
-			// compute frequency counts
-			int[] count = new int[R + 1];
-			for (int i = 0; i < n; i++)
-				count[a[i].charAt(d) + 1]++;
-
-			// compute cumulates
-			for (int r = 0; r < R; r++)
-				count[r + 1] += count[r];
-
-			// move data
-			for (int i = 0; i < n; i++)
-				aux[count[a[i].charAt(d)]++] = a[i];
-
-			// copy back
-			for (int i = 0; i < n; i++)
-				a[i] = aux[i];
-		}
-	}
+    /**.
+     * Constructs the object.
+     */
+    LSD() {
+        //constructor
+    }
+    public static void sort(final String[] a, final int w) {
+        /**.
+         * length of the given array
+         */
+        int n = a.length;
+        int R = 256;   // extend ASCII alphabet size
+        String[] aux = new String[n];
+        for (int d = w - 1; d >= 0; d--) {
+            // sort by key-indexed counting on dth character
+            // compute frequency counts
+            int[] count = new int[R + 1];
+            for (int i = 0; i < n; i++) {
+                count[a[i].charAt(d) + 1]++;
+            }
+            // compute cumulates
+            for (int r = 0; r < R; r++) {
+                count[r + 1] += count[r];
+            }
+            // move data
+            for (int i = 0; i < n; i++) {
+                aux[count[a[i].charAt(d)]++] = a[i];
+            }
+            // copy back
+            for (int i = 0; i < n; i++) {
+                a[i] = aux[i];
+            }
+        }
+    }
 }
