1facc1fe64245bc7d3e021179ed2324e1881b321
someshwarthuma <somesh.sandeep619@msitprogram.net>
2018-11-12 11:46:13 +0530

    "ads2_m10_a1_1: 4/9 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m10/Seam Carving/SeamCarver.java
+++ ADS-2-assignments/m10/Seam Carving/SeamCarver.java
@@ -1,6 +1,8 @@
 
 public class SeamCarver {
 	// create a seam carver object based on the given picture
+	int[][] edgeTo;
+	Double[][] distTo;
 	Picture picture;
 	int picHeight;
 	int picWidth;
@@ -30,26 +32,98 @@ public class SeamCarver {
 			return 1000;
 		}
 
-		double xRed = Math.abs(picture.get(x-1,y).getRed() - picture.get(x+1, y).getRed());
-		double xBlue = Math.abs(picture.get(x-1,y).getBlue() - picture.get(x+1, y).getBlue());
-		double xGreen = Math.abs(picture.get(x-1,y).getGreen() - picture.get(x+1, y).getGreen());
-		double yRed = Math.abs(picture.get(x,y -1).getRed() - picture.get(x, y + 1).getRed());
-		double yBlue = Math.abs(picture.get(x,y -1).getBlue() - picture.get(x, y + 1).getBlue());
-		double yGreen = Math.abs(picture.get(x,y -1).getGreen() - picture.get(x, y + 1).getGreen());
-		double total = xRed*xRed + xGreen*xGreen + xBlue*xBlue + yRed*yRed + yGreen*yGreen + yBlue*yBlue;
+		double xRed = Math.abs(picture.get(x - 1, y).getRed() - picture.get(x + 1, y).getRed());
+		double xBlue = Math.abs(picture.get(x - 1, y).getBlue() - picture.get(x + 1, y).getBlue());
+		double xGreen = Math.abs(picture.get(x - 1, y).getGreen() - picture.get(x + 1, y).getGreen());
+		double yRed = Math.abs(picture.get(x, y - 1).getRed() - picture.get(x, y + 1).getRed());
+		double yBlue = Math.abs(picture.get(x, y - 1).getBlue() - picture.get(x, y + 1).getBlue());
+		double yGreen = Math.abs(picture.get(x, y - 1).getGreen() - picture.get(x, y + 1).getGreen());
+		double total = xRed * xRed + xGreen * xGreen + xBlue * xBlue + yRed * yRed + yGreen * yGreen + yBlue * yBlue;
 		return Math.sqrt(total);
 	}
-
 	// sequence of indices for horizontal seam
 	public int[] findHorizontalSeam() {
+
+
 		return new int[0];
 	}
 
 	// sequence of indices for vertical seam
 	public int[] findVerticalSeam() {
-		return new int[0];
+		int[] indices = new int[picHeight];
+		edgeTo = new int[picHeight][picWidth];
+		distTo = new Double[picHeight][picWidth];
+		for ( int height = 0; height < distTo.length; height++) {
+			for (int width = 0; width < distTo[0].length; width++) {
+				distTo[height][width] = Double.MAX_VALUE;
+			}
+		}
+
+		for (int width = 0; width < picWidth ; width++) {
+			distTo[0][width] = 1000.0;
+		}
+
+
+		for (int height = 0; height < picHeight; height++) {
+			for (int width = 0 ; width < picWidth ; width++) {
+				relaxVertical(height , width);
+			}
+		}
+
+		Double minDist = Double.MAX_VALUE;
+		int minWidth = 0;
+		for(int width = 0; width < picWidth ; width++){
+			if(minDist > distTo[picHeight - 1][width]) {
+				minDist = distTo[picHeight-1][width];
+				minWidth = width;
+			}
+		}
+		int trace = minWidth;
+		for(int height = picHeight -1; height >= 0; height-- ) {
+			indices[height] = trace;
+			trace -= edgeTo[height][trace];
+		}
+		indices[0] = indices[1];
+		return indices;
 	}
 
+	private void relaxVertical(int row, int col) {
+		int nextRow = row + 1;
+        for (int i = -1; i <= 1; i++) {
+            int nextCol = col + i;
+            if (nextCol < 0 || nextCol >= picWidth) {
+            	continue;
+            }
+            if(i == 0) {
+            	if(distTo[nextRow][nextCol] >= distTo[row][col] + energy(nextCol, nextRow)) {
+            	distTo[nextRow][nextCol] = distTo[row][col] + energy(nextCol, nextRow);
+                edgeTo[nextRow][nextCol] = i;
+            	}
+            }
+            if (distTo[nextRow][nextCol] > distTo[row][col] + energy(nextCol, nextRow)) {
+                distTo[nextRow][nextCol] = distTo[row][col] + energy(nextCol, nextRow);
+                edgeTo[nextRow][nextCol] = i;
+            }
+
+		/*int nextHeight = height + 1;
+		int prevWidth = width - 1;
+		int nextWidth = width + 1;
+		Double distEnergy = distTo[height][width];
+		Double e1 = energy(nextHeight , prevWidth);
+		Double e2 = energy(nextHeight , width);
+		Double e3 = energy(nextHeight, nextWidth);
+		if (prevWidth >= 0 && nextWidth < picWidth && distTo[nextHeight][prevWidth] > distEnergy + e1) {
+			distTo[nextHeight][prevWidth] = e1;
+			edgeTo[nextHeight][nextWidth] = width;
+		} else if ((prevWidth >= 0) && (nextWidth < picWidth) && (distTo[nextHeight][nextWidth] > distEnergy + e3)) {
+			distTo[nextHeight][nextWidth] = e3;
+			edgeTo[nextHeight][nextWidth] = width;
+		} else if ((prevWidth >= 0 && nextWidth < picWidth && distTo[nextHeight][width] >= distEnergy + e2)) {
+		distTo[nextHeight][width] = e2;
+			edgeTo[nextHeight][width] = width;
+		}*/
+	}
+}
 	// remove horizontal seam from current picture
 	public void removeHorizontalSeam(int[] seam) {
 
