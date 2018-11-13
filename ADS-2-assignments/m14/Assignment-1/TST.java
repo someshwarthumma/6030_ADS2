e60fc862cd269106acc0b7ca3b9bf539ea2d3964
someshwarthuma <somesh.sandeep619@msitprogram.net>
2018-11-13 17:31:10 +0530

    "ads2_m14_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m14/Assignment-1/TST.java
+++ ADS-2-assignments/m14/Assignment-1/TST.java
@@ -1,65 +1,4 @@
-/******************************************************************************
- *  Compilation:  javac TST.java
- *  Execution:    java TST < words.txt
- *  Dependencies: StdIn.java
- *  Data files:   http://algs4.cs.princeton.edu/52trie/shellsST.txt
- *
- *  Symbol table with string keys, implemented using a ternary search
- *  trie (TST).
- *
- *
- *  % java TST < shellsST.txt
- *  keys(""):
- *  by 4
- *  sea 6
- *  sells 1
- *  she 0
- *  shells 3
- *  shore 7
- *  the 5
- *
- *  longestPrefixOf("shellsort"):
- *  shells
- *
- *  keysWithPrefix("shor"):
- *  shore
- *
- *  keysThatMatch(".he.l."):
- *  shells
- *
- *  % java TST
- *  theory the now is the time for all good men
- *
- *  Remarks
- *  --------
- *    - can't use a key that is the empty string ""
- *
- ******************************************************************************/
 
-//package edu.princeton.cs.algs4;
-
-/**
- *  The {@code TST} class represents an symbol table of key-value
- *  pairs, with string keys and generic values.
- *  It supports the usual <em>put</em>, <em>get</em>, <em>contains</em>,
- *  <em>delete</em>, <em>size</em>, and <em>is-empty</em> methods.
- *  It also provides character-based methods for finding the string
- *  in the symbol table that is the <em>longest prefix</em> of a given prefix,
- *  finding all strings in the symbol table that <em>start with</em> a given prefix,
- *  and finding all strings in the symbol table that <em>match</em> a given pattern.
- *  A symbol table implements the <em>associative array</em> abstraction:
- *  when associating a value with a key that is already in the symbol table,
- *  the convention is to replace the old value with the new value.
- *  Unlike {@link java.util.Map}, this class uses the convention that
- *  values cannot be {@code null}—setting the
- *  value associated with a key to {@code null} is equivalent to deleting the key
- *  from the symbol table.
- *  <p>
- *  This implementation uses a ternary search trie.
- *  <p>
- *  For additional documentation, see <a href="http://algs4.cs.princeton.edu/52trie">Section 5.2</a> of
- *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
- */
 public class TST<Value> {
     private int n;              // size
     private Node<Value> root;   // root of TST
@@ -70,16 +9,9 @@ public class TST<Value> {
         private Value val;                     // value associated with string
     }
 
-    /**
-     * Initializes an empty string symbol table.
-     */
     public TST() {
     }
 
-    /**
-     * Returns the number of key-value pairs in this symbol table.
-     * @return the number of key-value pairs in this symbol table
-     */
     public int size() {
         return n;
     }
@@ -91,7 +23,7 @@ public class TST<Value> {
      *     {@code false} otherwise
      * @throws IllegalArgumentException if {@code key} is {@code null}
      */
-    public boolean contains(String key) {
+    public boolean contains(final String key) {
         if (key == null) {
             throw new IllegalArgumentException("argument to contains() is null");
         }
@@ -105,7 +37,7 @@ public class TST<Value> {
      *     and {@code null} if the key is not in the symbol table
      * @throws IllegalArgumentException if {@code key} is {@code null}
      */
-    public Value get(String key) {
+    public Value get(final String key) {
         if (key == null) {
             throw new IllegalArgumentException("calls get() with null argument");
         }
@@ -114,15 +46,22 @@ public class TST<Value> {
         if (x == null) return null;
         return x.val;
     }
-
-    // return subtrie corresponding to given key
-    private Node<Value> get(Node<Value> x, String key, int d) {
+    /**
+     * { function_description }
+     *
+     * @param      x     { Node as X }
+     * @param      key   The key
+     * @param      d     { of int type for position }
+     *
+     * @return     { Node<Value> }
+     */
+    private Node<Value> get(final Node<Value> x, final String key, final int d) {
         if (x == null) return null;
         if (key.length() == 0) throw new IllegalArgumentException("key must have length >= 1");
         char c = key.charAt(d);
         if      (c < x.c)              return get(x.left,  key, d);
         else if (c > x.c)              return get(x.right, key, d);
-        else if (d < key.length() - 1) return get(x.mid,   key, d+1);
+        else if (d < key.length() - 1) return get(x.mid,   key, d + 1);
         else                           return x;
     }
 
@@ -134,15 +73,25 @@ public class TST<Value> {
      * @param val the value
      * @throws IllegalArgumentException if {@code key} is {@code null}
      */
-    public void put(String key, Value val) {
+    public void put(final String key, final Value val) {
         if (key == null) {
             throw new IllegalArgumentException("calls put() with null key");
         }
         if (!contains(key)) n++;
         root = put(root, key, val, 0);
     }
-
-    private Node<Value> put(Node<Value> x, String key, Value val, int d) {
+    /**
+     * put method for recursion
+     *
+     * @param      x1    The x 1
+     * @param      key   The key
+     * @param      val   The value
+     * @param      d     { for position of charactor }
+     *
+     * @return     { Node of type generic }
+     */
+    private Node<Value> put(final Node<Value> x1, final String key, final Value val, final int d) {
+        Node<Value> x = x1;
         char c = key.charAt(d);
         if (x == null) {
             x = new Node<Value>();
@@ -150,7 +99,7 @@ public class TST<Value> {
         }
         if      (c < x.c)               x.left  = put(x.left,  key, val, d);
         else if (c > x.c)               x.right = put(x.right, key, val, d);
-        else if (d < key.length() - 1)  x.mid   = put(x.mid,   key, val, d+1);
+        else if (d < key.length() - 1)  x.mid   = put(x.mid,   key, val, d + 1);
         else                            x.val   = val;
         return x;
     }
@@ -163,7 +112,7 @@ public class TST<Value> {
      *     or {@code null} if no such string
      * @throws IllegalArgumentException if {@code query} is {@code null}
      */
-    public String longestPrefixOf(String query) {
+    public String longestPrefixOf(final String query) {
         if (query == null) {
             throw new IllegalArgumentException("calls longestPrefixOf() with null argument");
         }
@@ -203,7 +152,7 @@ public class TST<Value> {
      *     as an iterable
      * @throws IllegalArgumentException if {@code prefix} is {@code null}
      */
-    public Iterable<String> keysWithPrefix(String prefix) {
+    public Iterable<String> keysWithPrefix(final String prefix) {
         if (prefix == null) {
             throw new IllegalArgumentException("calls keysWithPrefix() with null argument");
         }
@@ -214,9 +163,14 @@ public class TST<Value> {
         collect(x.mid, new StringBuilder(prefix), queue);
         return queue;
     }
-
-    // all keys in subtrie rooted at x with given prefix
-    private void collect(Node<Value> x, StringBuilder prefix, Queue<String> queue) {
+    /**
+     * method to collect
+     *
+     * @param      x       { Node<Value> }
+     * @param      prefix  The prefix
+     * @param      queue   The queue
+     */
+    private void collect(final Node<Value> x, final StringBuilder prefix, final Queue<String> queue) {
         if (x == null) return;
         collect(x.left,  prefix, queue);
         if (x.val != null) queue.enqueue(prefix.toString() + x.c);
@@ -233,20 +187,28 @@ public class TST<Value> {
      * @return all of the keys in the symbol table that match {@code pattern},
      *     as an iterable, where . is treated as a wildcard character.
      */
-    public Iterable<String> keysThatMatch(String pattern) {
+    public Iterable<String> keysThatMatch(final String pattern) {
         Queue<String> queue = new Queue<String>();
         collect(root, new StringBuilder(), 0, pattern, queue);
         return queue;
     }
- 
-    private void collect(Node<Value> x, StringBuilder prefix, int i, String pattern, Queue<String> queue) {
+    /**
+     * method to collect
+     *
+     * @param      x        { Node }
+     * @param      prefix   The prefix
+     * @param      i        { StringBuilder }
+     * @param      pattern  The pattern of type String
+     * @param      queue    The queue of type Queue
+     */
+    private void collect(final Node<Value> x, final StringBuilder prefix, final int i, final String pattern, final Queue<String> queue) {
         if (x == null) return;
         char c = pattern.charAt(i);
         if (c == '.' || c < x.c) collect(x.left, prefix, i, pattern, queue);
         if (c == '.' || c == x.c) {
             if (i == pattern.length() - 1 && x.val != null) queue.enqueue(prefix.toString() + x.c);
             if (i < pattern.length() - 1) {
-                collect(x.mid, prefix.append(x.c), i+1, pattern, queue);
+                collect(x.mid, prefix.append(x.c), i + 1, pattern, queue);
                 prefix.deleteCharAt(prefix.length() - 1);
             }
         }
@@ -254,26 +216,3 @@ public class TST<Value> {
     }
 }
 
-/******************************************************************************
- *  Copyright 2002-2016, Robert Sedgewick and Kevin Wayne.
- *
- *  This file is part of algs4.jar, which accompanies the textbook
- *
- *      Algorithms, 4th edition by Robert Sedgewick and Kevin Wayne,
- *      Addison-Wesley Professional, 2011, ISBN 0-321-57351-X.
- *      http://algs4.cs.princeton.edu
- *
- *
- *  algs4.jar is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  algs4.jar is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with algs4.jar.  If not, see http://www.gnu.org/licenses.
- ******************************************************************************/
