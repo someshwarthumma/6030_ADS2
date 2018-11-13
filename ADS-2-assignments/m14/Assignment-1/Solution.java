e1948596735f8fd2a246caab8e43d14d6de31a58
someshwarthuma <somesh.sandeep619@msitprogram.net>
2018-11-13 19:07:26 +0530

    "ads2_m14_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2-assignments/m14/Assignment-1/Solution.java
+++ ADS-2-assignments/m14/Assignment-1/Solution.java
@@ -3,46 +3,46 @@ import java.util.Scanner;
  * solution class
  */
 final class Solution {
-	/**.
-	 * constructor
-	 */
-	private Solution() {
-		//constructor
-	}
-	/**.
-	 * main method to handle the input
-	 * Complexity is L * N
-	 * L = length of word
-	 * N = No Of words
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		String[] words = loadWords();
-		//Your code goes here...
-		Scanner s = new Scanner(System.in);
-		String subtring = s.nextLine();
-		TST tst = new TST();
-		for (String each : words) {
-			int len = each.length();
-			for (int n = 0; n < each.length(); n++) {
-				tst.put(each.substring(n, len), 0);
-			}
-		}
-		/*for(String every: tst.keysWithPrefix(subtring)){
-			System.out.println(every);
-		}*/
-		System.out.println(tst.keysWithPrefix(subtring));
+    /**.
+     * constructor
+     */
+    private Solution() {
+        //constructor
+    }
+    /**.
+     * main method to handle the input
+     * Complexity is L * N
+     * L = length of word
+     * N = No Of words
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        String[] words = loadWords();
+        //Your code goes here...
+        Scanner s = new Scanner(System.in);
+        String subtring = s.nextLine();
+        TST tst = new TST();
+        for (String each : words) {
+            int len = each.length();
+            for (int n = 0; n < each.length(); n++) {
+                tst.put(each.substring(n, len), 0);
+            }
+        }
+        /*for(String every: tst.keysWithPrefix(subtring)){
+            System.out.println(every);
+        }*/
+        System.out.println(tst.keysWithPrefix(subtring));
 
-	}
-	/**.
-	 * load Words method
-	 * Complexity is O(N)
-	 *
-	 * @return     { String[] }
-	 */
-	public static String[] loadWords() {
-		In in = new In("/Files/dictionary-algs4.txt");
-		String[] words = in.readAllStrings();
-		return words;
-	}
+    }
+    /**.
+     * load Words method
+     * Complexity is O(N)
+     *
+     * @return     { String[] }
+     */
+    public static String[] loadWords() {
+        In in = new In("/Files/dictionary-algs4.txt");
+        String[] words = in.readAllStrings();
+        return words;
+    }
 }
