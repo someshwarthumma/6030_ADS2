3bcde1a218e416ef1c5e00653450f1a3c1092da8
someshwarthuma <somesh.sandeep619@msitprogram.net>
2018-11-15 20:00:22 +0530

    "ads2_m16_a1_1: 11/30 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS-2-assignments/m15/boggle/BoggleSolver.java
@@ -0,0 +1,118 @@
+import java.util.ArrayList;
+public class BoggleSolver {
+	// Initializes the data structure using the given array of strings as the dictionary.
+	// (You can assume each word in the dictionary contains only the uppercase letters A through Z.)
+	String[] dict;
+	int column;
+	int rows;
+	ArrayList<String> bag;
+	static TST<Integer> tst;
+	public BoggleSolver(final String[] dictionary) {
+		this.dict = dictionary;
+		tst = new TST<Integer>();
+		for(String each: dict){
+			tst.put(each,0);
+		}
+	}
+	// Returns the set of all valid words in the given Boggle board, as an Iterable.
+	public Iterable<String> getAllValidWords(BoggleBoard board) {
+		bag = new ArrayList<String>();
+		column = board.cols();
+		//BoggleBoard board1 = board;
+		rows = board.rows();
+		boolean[][] marked = new boolean[rows][column];
+		for(int i=0; i < rows ; i++){
+			for( int j=0; j< column; j++){
+				marked = new boolean[board.rows()][board.cols()];
+				dfs(board, i, j, board.getLetter(i, j)+"", marked);
+			}
+			marked = new boolean[board.rows()][board.cols()];
+		}
+		return bag;
+	}
+	private boolean isValid(String word){
+		Queue<String> temp = tst.keysWithPrefix(word);
+		if(temp.size()==0){
+			return false;
+		}
+		return true;
+	}
+	private boolean checkIndex(int i, int j){
+		if(i<0 || i >= rows || j <0 || j>= column){
+			return false;
+		}
+		return true;
+	}
+
+	private void dfs(BoggleBoard board, int i, int j, String word, boolean[][] marked){
+		if(i<0 || i >= rows || j <0 || j>= column){
+			return;
+		}
+		marked[i][j] = true;
+		if(tst.contains(word) && !bag.contains(word) && word.length() >2){
+			bag.add(word);
+		}
+		if(isValid(word)){
+			if(checkIndex(i+1,j+1) && !marked[i+1][j+1]){
+				dfs(board, i+1, j+1, word+board.getLetter(i+1, j+1), marked);
+				marked[i+1][j+1] = false;
+			}
+			if(checkIndex(i-1, j-1) && !marked[i-1][j-1]){
+				dfs(board, i-1, j-1, word+board.getLetter(i-1, j-1), marked);
+				marked[i-1][j-1] = false;
+			}
+			if(checkIndex(i-1, j+1) && !marked[i-1][j+1]){
+				dfs(board, i-1, j+1, word+board.getLetter(i-1, j+1), marked);
+				marked[i-1][j+1] = false;
+			}
+			if(checkIndex(i+1, j-1) && !marked[i+1][j-1]){
+				dfs(board, i+1, j+1, word+board.getLetter(i+1, j-1), marked);
+				marked[i+1][j-1] = false;
+			}
+			if(checkIndex(i-1, j) && !marked[i-1][j]){
+				dfs(board, i-1, j, word+board.getLetter(i-1, j), marked);
+				marked[i-1][j] = false;
+			}
+
+			if(checkIndex(i+1, j) && !marked[i+1][j]){
+				dfs(board, i+1, j, word+board.getLetter(i+1, j), marked);
+				marked[i+1][j] = false;
+			}
+			if(checkIndex(i, j+1) && !marked[i][j+1]){
+				dfs(board, i, j+1, word+board.getLetter(i, j+1), marked);
+				marked[i][j+1] = false;
+			}
+			if(checkIndex(i, j-1) && !marked[i][j-1]){
+				dfs(board, i, j-1, word+board.getLetter(i, j-1), marked);
+				marked[i][j-1] =false;
+			}
+			
+			
+			
+
+		}
+		marked[i][j]= false;
+
+		//marked[i][j] = true;
+	}
+	//private dfs(Grapgh g, String source)
+	// Returns the score of the given word if it is in the dictionary, zero otherwise.
+	// (You can assume the word contains only the uppercase letters A through Z.)
+	public int scoreOf(String word) {
+		int len = word.length();
+		if(len>=8){
+			return 11;
+		} else if(len==7){
+			return 5;
+		} else if(len==6){
+			return 3;
+		} else if(len==5){
+			return 2;
+		} else if(len>=3){
+			return 1;
+		} else if(len>=0){
+			return 0;
+		}
+		return 0;
+	}
+}
---
+++ ADS-2-assignments/m15/boggle/TST.java
@@ -0,0 +1,330 @@
+/**.
+ * tst class
+ *
+ * @param      <Value>  The value
+ */
+public class TST<Value> {
+    /**.
+     * variable for size
+     */
+    private int n;
+    /**.
+     * variable for root
+     */
+    private Node<Value> root;
+    /**.
+     * Node class
+     *
+     * @param      <Value>  The value
+     */
+    private static class Node<Value> {
+        /**.
+         * variable for character
+         */
+        private char c;
+        /**.
+         * Node variable as left, mid, right
+         */
+        private Node<Value> left, mid, right;
+        /**.
+         * val variable
+         */
+        private Value val;
+    }
+    /**.
+     * TSt constructor
+     */
+    public TST() {
+    }
+    /**.
+     * getter method for size
+     *
+     * @return     { int }
+     */
+    public int size() {
+        return n;
+    }
+
+    /**.
+     * Does this symbol table contain the given key?
+     * Complexity is O(L + Log(N))
+     * L= length of the string
+     * N = size of tst
+     * @param key the key
+     * @return {@code true} if this symbol table contains {@code key} and
+     *     {@code false} otherwise
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public boolean contains(final String key) {
+        if (key == null) {
+            throw new IllegalArgumentException(
+                "argument to contains() is null");
+        }
+        return get(key) != null;
+    }
+
+    /**.
+     * Returns the value associated with the given key.
+     * Complexity is O(L + Log(N))
+     * L= length of the string
+     * N = no of inputs
+     * @param key the key
+     * @return the value associated with the
+     * given key if the key is in the symbol table
+     *     and {@code null} if the key is not in the symbol table
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public Value get(final String key) {
+        if (key == null) {
+            throw new IllegalArgumentException(
+                "calls get() with null argument");
+        }
+        if (key.length() == 0) {
+            throw new IllegalArgumentException(
+                "key must have length >= 1");
+        }
+        Node<Value> x = get(root, key, 0);
+        if (x == null) {
+            return null;
+        }
+        return x.val;
+    }
+    /**.
+     * { method to get the vlaue }
+     * Complexity is O(L + Log(N))
+     * L= length of the string
+     * N = no of inputs
+     *
+     * @param      x     { Node as X }
+     * @param      key   The key
+     * @param      d     { of int type for position }
+     *
+     * @return     { Node<Value> }
+     */
+    private Node<Value> get(final Node<Value> x,
+        final String key, final int d) {
+        if (x == null) {
+            return null;
+        }
+        if (key.length() == 0) {
+            throw new IllegalArgumentException("key must have length >= 1");
+        }
+        char c = key.charAt(d);
+        if (c < x.c) {
+            return get(x.left,  key, d);
+        } else if (c > x.c) {
+            return get(x.right, key, d);
+        } else if (d < key.length() - 1) {
+            return get(x.mid,   key, d + 1);
+        } else {
+            return x;
+        }
+    }
+
+    /**.
+     * Inserts the key-value pair into the symbol table
+     * overwriting the old value
+     * with the new value if the key is already in the symbol table.
+     * If the value is {@code null}, this effectively
+     * deletes the key from the symbol table.
+     * Complexity is O(L + Log(N))
+     * L= length of the string
+     * N = no of inputs
+     * @param key the key
+     * @param val the value
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public void put(final String key, final Value val) {
+        if (key == null) {
+            throw new IllegalArgumentException("calls put() with null key");
+        }
+        if (!contains(key)) {
+            n++;
+        }
+        root = put(root, key, val, 0);
+    }
+    /**.
+     * put method for recursion
+     * Complexity is O(L + Log(N))
+     * L= length of the string
+     * N = no of inputs
+     *
+     * @param      x1    The x 1
+     * @param      key   The key
+     * @param      val   The value
+     * @param      d     { for position of charactor }
+     *
+     * @return     { Node of type generic }
+     */
+    private Node<Value> put(final Node<Value> x1, final String key,
+        final Value val, final int d) {
+        Node<Value> x = x1;
+        char c = key.charAt(d);
+        if (x == null) {
+            x = new Node<Value>();
+            x.c = c;
+        }
+        if      (c < x.c) {
+            x.left  = put(x.left,  key, val, d);
+        } else if (c > x.c) {
+            x.right = put(x.right, key, val, d);
+        } else if (d < key.length() - 1) {
+            x.mid   = put(x.mid,   key, val, d + 1);
+        } else {
+            x.val   = val;
+        }
+        return x;
+    }
+
+    /**.
+     * Returns the string in the symbol table that is
+     * the longest prefix of {@code query},
+     * or {@code null}, if no such string.
+     * Complexity is O(L)
+     * L = length of the query
+     * @param query the query string
+     * @return the string in the symbol table
+     * that is the longest prefix of {@code query},
+     *     or {@code null} if no such string
+     * @throws IllegalArgumentException if {@code query} is {@code null}
+     */
+    public String longestPrefixOf(final String query) {
+        if (query == null) {
+            throw new IllegalArgumentException(
+                "calls longestPrefixOf() with null argument");
+        }
+        if (query.length() == 0) {
+            return null;
+        }
+        int length = 0;
+        Node<Value> x = root;
+        int i = 0;
+        while (x != null && i < query.length()) {
+            char c = query.charAt(i);
+            if      (c < x.c) {
+                x = x.left;
+            } else if (c > x.c) {
+                x = x.right;
+            } else {
+                i++;
+                if (x.val != null) {
+                    length = i;
+                }
+                x = x.mid;
+            }
+        }
+        return query.substring(0, length);
+    }
+
+    /**.
+     * Returns all keys in the symbol table as an {@code Iterable}.
+     * To iterate over all of the keys in the symbol table named {@code st},
+     * use the foreach notation: {@code for (Key key : st.keys())}.
+     * @return all keys in the symbol table as an {@code Iterable}
+     */
+    public Iterable<String> keys() {
+        Queue<String> queue = new Queue<String>();
+        collect(root, new StringBuilder(), queue);
+        return queue;
+    }
+
+    /**.
+     * Returns all of the keys in the set that start with {@code prefix}.
+     * Complexity is O(L + Log(N))
+     * L= length of the string
+     * N = no of inputs
+     * @param prefix the prefix
+     * @return all of the keys in the set that start with {@code prefix},
+     *     as an iterable
+     * @throws IllegalArgumentException if {@code prefix} is {@code null}
+     */
+    public Queue<String> keysWithPrefix(final String prefix) {
+        if (prefix == null) {
+            throw new IllegalArgumentException(
+                "calls keysWithPrefix() with null argument");
+        }
+        Queue<String> queue = new Queue<String>();
+        Node<Value> x = get(root, prefix, 0);
+        if (x == null) {
+            return queue;
+        }
+        if (x.val != null) {
+            queue.enqueue(prefix);
+        }
+        collect(x.mid, new StringBuilder(prefix), queue);
+        return queue;
+    }
+    /**.
+     * method to collect
+     * Complexity is O(L + Log(N))
+     * L= length of the string
+     * N = no of inputs
+     *
+     * @param      x       { Node<Value> }
+     * @param      prefix  The prefix
+     * @param      queue   The queue
+     */
+    private void collect(final Node<Value> x, final StringBuilder prefix,
+        final Queue<String> queue) {
+        if (x == null) {
+            return;
+        }
+        collect(x.left,  prefix, queue);
+        if (x.val != null) {
+            queue.enqueue(prefix.toString() + x.c);
+        }
+        collect(x.mid,   prefix.append(x.c), queue);
+        prefix.deleteCharAt(prefix.length() - 1);
+        collect(x.right, prefix, queue);
+    }
+
+
+    /**.
+     * Returns all of the keys in the symbol table that match {@code pattern},
+     * where . symbol is treated as a wildcard character.
+     * Complexity is O(L + Log(N))
+     * L= length of the string
+     * N = no of inputs
+     * @param pattern the pattern
+     * @return all of the keys in the symbol table that match {@code pattern},
+     *     as an iterable, where . is treated as a wildcard character.
+     */
+    public Iterable<String> keysThatMatch(final String pattern) {
+        Queue<String> queue = new Queue<String>();
+        collect(root, new StringBuilder(), 0, pattern, queue);
+        return queue;
+    }
+    /**.
+     * method to collect
+     * Complexity is O(L + Log(N))
+     * L= length of the string
+     * N = no of inputs
+     * @param      x        { Node }
+     * @param      prefix   The prefix
+     * @param      i        { StringBuilder }
+     * @param      pattern  The pattern of type String
+     * @param      queue    The queue of type Queue
+     */
+    private void collect(final Node<Value> x, final StringBuilder prefix,
+        final int i, final String pattern, final Queue<String> queue) {
+        if (x == null) {
+            return;
+        }
+        char c = pattern.charAt(i);
+        if (c == '.' || c < x.c) {
+            collect(x.left, prefix, i, pattern, queue);
+        }
+        if (c == '.' || c == x.c) {
+            if (i == pattern.length() - 1 && x.val != null) {
+                queue.enqueue(prefix.toString() + x.c);
+            }
+            if (i < pattern.length() - 1) {
+                collect(x.mid, prefix.append(x.c), i + 1, pattern, queue);
+                prefix.deleteCharAt(prefix.length() - 1);
+            }
+        }
+        if (c == '.' || c > x.c) {
+            collect(x.right, prefix, i, pattern, queue);
+        }
+    }
+}
